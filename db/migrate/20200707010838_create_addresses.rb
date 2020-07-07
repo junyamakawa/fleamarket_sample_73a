7f4e385182957bd7d22f9cdeebc3a232fab2f34c
mamoru622 <info@takadamamoru.com>
2020-07-07 11:09:25 +0900

    Update 20200707010838_create_addresses.rb
    ,が抜けていたので訂正した

--- db/migrate/20200707010838_create_addresses.rb
+++ db/migrate/20200707010838_create_addresses.rb
@@ -7,9 +7,9 @@ class CreateAddresses < ActiveRecord::Migration[5.2]
       t.string :first_name_kana,    null: false
       t.integer :post_code,         null: false
       t.string :prefecture,         null: false
-      t.string :city                null: false
-      t.string :house_number        null: false
-      t.string :building_name       
+      t.string :city,                null: false
+      t.string :house_number,        null: false
+      t.string :building_name
       t.string :phone_number
       t.references :user
       t.timestamps
