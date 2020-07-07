52168f5dcad4b9c73a5b8f03b671e82f2a29a0b1
mamoru622 <info@takadamamoru.com>
2020-07-07 10:26:33 +0900

    Update address.rb
    Userテーブルとのアソシエーションの定義とバリデーションを設定した

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,5 @@
 class Address < ApplicationRecord
+  belongs_to :user, optional: true
+  validates :family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :prefecture, :city ,:house_number ,presence: true
 end
+
