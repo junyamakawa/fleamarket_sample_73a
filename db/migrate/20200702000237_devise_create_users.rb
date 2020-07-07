00c1c200bdb4ca61f40067c6c6024e1f4bfc09ce
mamoru622 <info@takadamamoru.com>
2020-07-07 10:24:56 +0900

    Update 20200702000237_devise_create_users.rb
    nullのスペルミスを訂正した

--- db/migrate/20200702000237_devise_create_users.rb
+++ db/migrate/20200702000237_devise_create_users.rb
@@ -4,16 +4,16 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nickname,           nulll: false
+      t.string :nickname,           null: false
       t.string :email,              null: false
       t.string :encrypted_password, null: false
-      t.string :family_name,        nulll: false
-      t.string :first_name,         nulll: false
-      t.string :family_name_kana,   nulll: false
-      t.string :first_name_kana,    nulll: false
-      t.date :birth_date,           nulll: false
-      t.string :phone_number,       nulll: false, unique: true
-      t.text :introduction,         nulll: false
+      t.string :family_name,        null: false
+      t.string :first_name,         null: false
+      t.string :family_name_kana,   null: false
+      t.string :first_name_kana,    null: false
+      t.date :birth_date,           null: false
+      t.string :phone_number,       null: false, unique: true
+      t.text :introduction,         null: false
 
       ## Recoverable
       t.string   :reset_password_token
