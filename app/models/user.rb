948110a4afe9bd1b83267c4c8f72ba104e122ce2
mamoru622 <info@takadamamoru.com>
2020-07-08 10:22:53 +0900

    Update user.rb
    バリデーションを追加した。
    ・名前は全角入力
    ・フリガナはカタカナ全角入力

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
   has_one :address
 
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_date, presence: true
   validates :phone_number, presence: true, uniqueness: true
 
