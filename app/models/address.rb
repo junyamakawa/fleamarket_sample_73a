7423d3d1863dcde36b017c4637c302dc61b9e940
mamoru622 <info@takadamamoru.com>
2020-07-08 10:24:41 +0900

    Update address.rb
    バリデーションを変更した
    ・名前は全角入力
    ・フリガナは全角カタカナ入力

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,9 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
-  validates :family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :prefecture, :city ,:house_number ,presence: true
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :post_code, :prefecture, :city ,:house_number ,presence: true
 end
 
