fd231655d36b594bbfa955b5ce26885eb3840d98
pregabalin75mg <tsubasa1117@mbr.nifty.com>
2020-07-12 23:43:54 +0900

    Add public_key
    公開鍵を記入
    この後実装する「トークン」の暗号化をするため

--- app/assets/javascripts/credit_cards.js
+++ app/assets/javascripts/credit_cards.js
@@ -2,5 +2,5 @@ window.addEventListener('DOMContentLoaded', function(){
 
   let submit = document.getElementById("payment_card_submit-button");
 
-
+  Payjp.setPublicKey('pk_test_325c074a2cb4d1dd00d35cba'); 
 });
