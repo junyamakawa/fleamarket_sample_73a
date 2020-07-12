9c0394570bc13de1b51614ea0c59a8c7bb0e6ad1
pregabalin75mg <tsubasa1117@mbr.nifty.com>
2020-07-12 18:04:26 +0900

    create customer data
    Pay.jp にある customer テーブルに3つのデータを作成し、
    それを変数 customer にした。

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -9,6 +9,14 @@ class CreditCardsController < ApplicationController
 
     if params["payjp_token"].blank?
       redirect_to action: "new", alert: "クレジットカードを登録できませんでした。"
+    else
+      customer = Payjp::Customer.create(
+        email: current_user.email,
+        card: params["payjp_token"],
+        metadata: {user_id: current_user.id} #最悪なくてもOK！
+      )
+
+      
     end
 
   end
