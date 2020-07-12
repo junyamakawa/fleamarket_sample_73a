2c22fb34ace210fea910e82a114ec81dc43c420e
pregabalin75mg <tsubasa1117@mbr.nifty.com>
2020-07-12 14:36:53 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_12_033305) do
+ActiveRecord::Schema.define(version: 2020_07_12_045621) do
+
+  create_table "credit_cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_credit_cards_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname"
@@ -33,4 +42,5 @@ ActiveRecord::Schema.define(version: 2020_07_12_033305) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "credit_cards", "users"
 end
