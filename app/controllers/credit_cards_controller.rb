491f8785c9dbc5105e7115ef478cde7965abcbae
pregabalin75mg <tsubasa1117@mbr.nifty.com>
2020-07-12 18:13:31 +0900

    save @card
    作成した @card を保存

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -17,8 +17,9 @@ class CreditCardsController < ApplicationController
       )
 
       @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+      @card.save
 
-      
+
     end
 
   end
