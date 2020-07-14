cdad2524c7a9d532cf529b902e9dae1d2d4995a2
junyamakawa <udonyamakawa@gmail.com>
2020-07-14 13:51:24 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'users#index'
+  resources :products, only: :index
 end
