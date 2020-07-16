31647d65d6e901451d3d82841909356305e331d5
pregabalin75mg <tsubasa1117@mbr.nifty.com>
2020-07-16 22:53:21 +0900

    has_many :credit_cards, dependent: :destroy
    書き漏れていた users モデルの dependent: :destroy を追加した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :credit_cards
+  has_many :credit_cards, dependent: :destroy
 end
