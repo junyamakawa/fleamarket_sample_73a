8b12b093e83ed289172fbad44a3b43d8977d8857
pregabalin75mg <tsubasa1117@mbr.nifty.com>
2020-07-21 22:00:09 +0900

    Set to move to show-action after registration
    クレジットカード登録後に show アクションへ移行するよう設定。

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -19,6 +19,8 @@ class CreditCardsController < ApplicationController
 
       @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       @card.save
+
+      redirect_to credit_card_path(current_user.id)
     end
 
   end
