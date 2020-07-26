5337eb7487c0e429d1b3bda666f27631e3a06d66
pregabalin75mg <tsubasa1117@mbr.nifty.com>
2020-07-26 22:27:27 +0900

    Create branch condition
    在庫がある場合と無い場合で条件を分岐させた。

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -60,14 +60,19 @@ class CreditCardsController < ApplicationController
   def buy
     @product = Product.find(params[:product_id])
 
+
     if user_signed_in?
+      if @product.status == "sale"
       @user = current_user
       @user.credit_cards.present?
       @card = CreditCard.find_by(user_id: current_user.id)
       customer = Payjp::Customer.retrieve(@card.customer_id)
       @customer_card = customer.cards.retrieve(@card.card_id)
+      else 
+        redirect_to root_path,alert: "売り切れています"
+      end
 
-      
+
     end
   end
 
@@ -75,6 +80,7 @@ class CreditCardsController < ApplicationController
     @product = Product.find(params[:product_id])
 
     @product.with_lock do
+      if @product.status == "sale"
         @card = CreditCard.find_by(user_id: current_user.id)
         charge = Payjp::Charge.create(
           amount: @product.price,
@@ -83,7 +89,9 @@ class CreditCardsController < ApplicationController
         )
         @product.update( status: 1 )
         redirect_to root_path, alert: "購入しました。"
-        redirect_to  new_credit_card_path, alert: "クレジット登録してください。"
+      else
+        redirect_to root_path, alert: "売り切れています。"
+      end
     end
   end
 
