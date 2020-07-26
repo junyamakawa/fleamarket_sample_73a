55119962e41d049e4e40526aa15f05a0298a4aa1
pregabalin75mg <tsubasa1117@mbr.nifty.com>
2020-07-26 22:30:51 +0900

    change to turbolinks:load from DOMContentLoaded
    addEventListener を turbolinks:load に変更。
    Uncaught TypeError: Cannot read property 'addEventListener' of null　というエラーをなくすためだが、結局エラーは無くならず。

--- app/assets/javascripts/credit_cards.js
+++ app/assets/javascripts/credit_cards.js
@@ -1,4 +1,4 @@
-window.addEventListener("DOMContentLoaded", function(){
+window.addEventListener("turbolinks:load", function(){
 
   let submit = document.getElementById("payment_card_submit-button");
 
