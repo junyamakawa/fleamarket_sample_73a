db997df77fc4aea11207210b6ef613793d229d25
mamoru622 <info@takadamamoru.com>
2020-07-29 08:30:32 +0900

    Update products_controller.rb
    商品詳細を表示するためのshowアクションを定義した。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,5 +5,13 @@ class ProductsController < ApplicationController
 
   def new
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
   
+  private
+  def product_params
+    params.require(:product).permit(:name, :description, :brand, :condition_id, :delivery_cost_id, :region_id, :preparation_day_id, :price, :user_id, :status)
+  end
 end
