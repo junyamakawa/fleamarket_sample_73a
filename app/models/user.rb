6f739b72ac4fa5161835388fa793f68fa49c593b
mamoru622 <info@takadamamoru.com>
2020-07-29 09:35:19 +0900

    Update user.rb
    productテーブルとのアソシエーションを定義した

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :credit_cards, dependent: :destroy
-  
+  has_many :products, dependent: :destroy
   has_one :address
 
   validates :nickname, presence: true
