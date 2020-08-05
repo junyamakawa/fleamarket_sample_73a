e3cc5c89e8884e5e14572194bd34c9a35348975f
junyamakawa <udonyamakawa@gmail.com>
2020-08-05 17:29:52 +0900

    Update products_controller.rb
    ・商品の削除
    ・ユーザの商品一覧へリダイレクト
    ・フラッシュメッセージ表示

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,9 +6,14 @@ class ProductsController < ApplicationController
   def new
   end
 
+  def show
+  end
+
+
   def destroy 
     product = @product.find(params[:id])
     product.destroy
+    redirect_to product_path(params[:user_id]), notice: "商品を削除しました"
   end
   
 end
