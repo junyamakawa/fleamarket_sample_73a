898ba02001bf3ec524e70eca9be392b97948a25a
pregabalin75mg <tsubasa1117@mbr.nifty.com>
2020-08-09 18:23:44 +0900

    copy and paste
    商品出品機能のブランチからコードを拝借

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,44 @@
 class ProductsController < ApplicationController
-  before_action :move_to_login, only: [:new]
+  # before_action :move_to_login, only: [:new]
 
   def index 
     @products = Product.where(status: 0)
+    @image = Image.all
   end
 
   def new
+    @product = Product.new
+    @product.images.new
+    # @product.categories.new
   end
 
-  def show
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+      logger.debug @product.errors.inspect
+    end
   end
-  
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :category_id, :brand, :condition_id, :delivery_cost_id, :region_id, :preparation_day_id, :price, images_attributes: [:src], categories_attributes: [:category_name]).merge(user_id: current_user.id)  
+  end
+    
+
   def move_to_login
     redirect_to new_user_session_path unless user_signed_in?
   end
