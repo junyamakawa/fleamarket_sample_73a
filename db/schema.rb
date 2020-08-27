09dc8e8fe0be5a5c88ae1168886577e4ab95d44b
mamoru622 <info@takadamamoru.com>
2020-08-28 07:10:14 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_22_044058) do
+ActiveRecord::Schema.define(version: 2020_08_22_043130) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "family_name", null: false
@@ -53,19 +53,11 @@ ActiveRecord::Schema.define(version: 2020_08_22_044058) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "product_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "product_id", null: false
-    t.bigint "category_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_product_categories_on_category_id"
-    t.index ["product_id"], name: "index_product_categories_on_product_id"
-  end
-
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
     t.string "brand"
+    t.integer "category_id"
     t.integer "condition_id"
     t.integer "delivery_cost_id"
     t.integer "region_id"
