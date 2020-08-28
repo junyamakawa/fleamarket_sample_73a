69280c0bca5353208ec85747fd28ae08dd93f0eb
mamoru622 <info@takadamamoru.com>
2020-08-28 17:41:57 +0900

    Update product.rb
    productテーブルとcategoryテーブルのアソシエーションを定義した

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,8 @@ class Product < ApplicationRecord
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   belongs_to :user
+  has_many :product_categories, dependent: :destroy
+  has_many :categories, through: :product_categories
   belongs_to_active_hash :condition
   belongs_to_active_hash :preparation_day
   belongs_to_active_hash :region
